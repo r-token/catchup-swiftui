e83e9d900c532b537f5f5879188c11700d5885d8
Ryan Token <ryantoken13@icloud.com>
2020-04-18 01:51:35 -0500

    Got all notifications working - including automatically adding birthdays and anniversaries. Also centered the Time Picker on the Preference screen, made phone numbers and email addresses tappable, and got contact pictures to show up correctly

--- SelectedContact+CoreDataClass.swift
+++ SelectedContact+CoreDataClass.swift
@@ -2,7 +2,7 @@
 //  SelectedContact+CoreDataClass.swift
 //  CatchUp-SwiftUI
 //
-//  Created by Ryan Token on 4/14/20.
+//  Created by Ryan Token on 4/17/20.
 //  Copyright © 2020 Token Solutions. All rights reserved.
 //
 //
@@ -11,6 +11,6 @@ import Foundation
 import CoreData
 
 @objc(SelectedContact)
-public class SelectedContact: NSManagedObject, Identifiable {
+public class SelectedContact: NSManagedObject {
 
 }
--- SelectedContact+CoreDataProperties.swift
+++ SelectedContact+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  SelectedContact+CoreDataProperties.swift
 //  CatchUp-SwiftUI
 //
-//  Created by Ryan Token on 4/14/20.
+//  Created by Ryan Token on 4/17/20.
 //  Copyright © 2020 Token Solutions. All rights reserved.
 //
 //
@@ -17,24 +17,26 @@ extension SelectedContact {
         return NSFetchRequest<SelectedContact>(entityName: "SelectedContact")
     }
 
-    @NSManaged public var address: String
-    @NSManaged public var anniversary: String
-    @NSManaged public var birthday: String
-    @NSManaged public var email: String
-    @NSManaged public var id: UUID
-    @NSManaged public var name: String
-    @NSManaged public var notification_identifier: UUID
+    @NSManaged public var address: String?
+    @NSManaged public var anniversary: String?
+    @NSManaged public var anniversary_notification_id: UUID?
+    @NSManaged public var birthday: String?
+    @NSManaged public var birthday_notification_id: UUID?
+    @NSManaged public var email: String?
+    @NSManaged public var id: UUID?
+    @NSManaged public var name: String?
+    @NSManaged public var notification_identifier: UUID?
     @NSManaged public var notification_preference: Int16
-    @NSManaged public var phone: String
-    @NSManaged public var picture: String
-    @NSManaged public var secondary_address: String
-    @NSManaged public var secondary_email: String
-    @NSManaged public var secondary_phone: String
     @NSManaged public var notification_preference_hour: Int16
     @NSManaged public var notification_preference_minute: Int16
-    @NSManaged public var notification_preference_customdate: Date
-    @NSManaged public var birthday_notification_id: UUID
-    @NSManaged public var anniversary_notification_id: UUID
     @NSManaged public var notification_preference_weekday: Int16
+    @NSManaged public var phone: String?
+    @NSManaged public var picture: String?
+    @NSManaged public var secondary_address: String?
+    @NSManaged public var secondary_email: String?
+    @NSManaged public var secondary_phone: String?
+    @NSManaged public var notification_preference_custom_month: Int16
+    @NSManaged public var notification_preference_custom_day: Int16
+    @NSManaged public var notification_preference_custom_year: Int16
 
 }
