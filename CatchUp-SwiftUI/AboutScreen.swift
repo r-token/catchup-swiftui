dbf0e1e74c75ba56bbcaa77aea896a98f9b21348
Ryan Token <ryantoken13@icloud.com>
2020-04-26 10:36:31 -0500

    Removed the 'Done' button from the About screen, it was causing some trouble on dismissal

--- CatchUp-SwiftUI/AboutScreen.swift
+++ CatchUp-SwiftUI/AboutScreen.swift
@@ -10,108 +10,93 @@ import SwiftUI
 
 struct AboutScreen: View {
 	@State private var showingAlert = false
-	@Environment(\.presentationMode) var presentationMode
 	
 	let generator = UINotificationFeedbackGenerator()
 	
     var body: some View {
-		VStack(alignment: .center) {
+		VStack(alignment: .center, spacing: 15) {
+				
+			Spacer()
+			
+			Image("CatchUp")
+				.resizable()
+				.frame(width: 80, height: 80)
+				.clipShape(RoundedRectangle(cornerRadius: 15))
+				.shadow(radius: 10)
+			
+			Text("CatchUp")
+				.foregroundColor(.orange)
+				.font(.largeTitle)
+				.bold()
+			
+			Text("Made with ❤️ by an independent developer")
+				.multilineTextAlignment(.center)
+				.padding(.horizontal)
+			
+			.padding(.bottom)
+			
+			Divider()
+				.frame(height: 1)
+				.background(Color.orange)
+				.padding(.bottom)
+			
+			Text("Tip Jar")
+				.font(.headline)
+			
+			Text("CatchUp is free with no ads. If you find it useful, please consider supporting development by leaving a tip.")
+				.multilineTextAlignment(.center)
+			
+			.padding(.bottom)
 			
 			HStack {
+				
 				Spacer()
-				Button("Done") {
-					self.presentationMode.wrappedValue.dismiss()
+				
+				Button("$0.99") {
+					self.graciousTipPressed()
 				}
-				.foregroundColor(.blue)
-				.font(.headline)
-				.padding(.top)
-				.padding(.trailing)
+					.font(.headline)
+					.foregroundColor(.white)
+					.padding()
+					.background(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .top, endPoint: .bottom))
+					)
+					.shadow(radius: 15)
+				
+				Spacer()
+				
+				Button("$1.99") {
+					self.generousTipPressed()
+				}
+					.font(.headline)
+					.foregroundColor(.white)
+					.padding()
+					.background(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .top, endPoint: .bottom))
+					)
+					.shadow(radius: 15)
+				
+				Spacer()
+				
+				Button("$4.99") {
+					self.gratuitousTipPressed()
+				}
+					.font(.headline)
+					.foregroundColor(.white)
+					.padding()
+					.background(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .top, endPoint: .bottom))
+					)
+					.shadow(radius: 15)
+				
+				Spacer()
 			}
 			
-			VStack(alignment: .center, spacing: 15) {
-				
-				Spacer()
-				
-				Image("CatchUp")
-					.resizable()
-					.frame(width: 80, height: 80)
-					.clipShape(RoundedRectangle(cornerRadius: 15))
-					.shadow(radius: 10)
-				
-				Text("CatchUp")
-					.foregroundColor(.orange)
-					.font(.largeTitle)
-					.bold()
-				
-				Text("Made with ❤️ by an independent developer")
-					.multilineTextAlignment(.center)
-					.padding(.horizontal)
-				
-				.padding(.bottom)
-				
-				Divider()
-					.frame(height: 1)
-					.background(Color.orange)
-					.padding(.bottom)
-				
-				Text("Tip Jar")
-					.font(.headline)
-				
-				Text("CatchUp is free with no ads. If you find it useful, please consider supporting development by leaving a tip.")
-					.multilineTextAlignment(.center)
-				
-				.padding(.bottom)
-				
-				HStack {
-					
-					Spacer()
-					
-					Button("$0.99") {
-						self.graciousTipPressed()
-					}
-						.font(.headline)
-						.foregroundColor(.white)
-						.padding()
-						.background(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .top, endPoint: .bottom))
-						)
-						.shadow(radius: 15)
-					
-					Spacer()
-					
-					Button("$1.99") {
-						self.generousTipPressed()
-					}
-						.font(.headline)
-						.foregroundColor(.white)
-						.padding()
-						.background(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .top, endPoint: .bottom))
-						)
-						.shadow(radius: 15)
-					
-					Spacer()
-					
-					Button("$4.99") {
-						self.gratuitousTipPressed()
-					}
-						.font(.headline)
-						.foregroundColor(.white)
-						.padding()
-						.background(RoundedRectangle(cornerRadius: 20).fill(LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .top, endPoint: .bottom))
-						)
-						.shadow(radius: 15)
-					
-					Spacer()
-				}
-				
-				.padding(.bottom)
-				.padding(.bottom)
-				
-				Divider()
-					.frame(height: 1)
-					.background(Color.orange)
-				
-				Spacer()
-			}
+			.padding(.bottom)
+			.padding(.bottom)
+			
+			Divider()
+				.frame(height: 1)
+				.background(Color.orange)
+			
+			Spacer()
 		}
 		.padding()
 		.onAppear(perform: fetchAvailableIAPs)
@@ -138,9 +123,7 @@ struct AboutScreen: View {
 }
 
 struct AboutScreen_Previews: PreviewProvider {
-	@Environment(\.presentationMode) var presentationMode
-	
-    static var previews: some View {
+	static var previews: some View {
         AboutScreen()
     }
 }
