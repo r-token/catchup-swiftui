d673871cf260bac942738cfb6d9a36ab4e3edff3
Ryan Token <ryantoken13@gmail.com>
2024-04-14 16:31:55 -0600

    Add a previewContainer for Xcode previews

--- CatchUp-SwiftUI/Data/DataController.swift
+++ CatchUp-SwiftUI/Data/DataController.swift
@@ -7,8 +7,26 @@
 //
 
 import Foundation
+import SwiftData
 
 @Observable
 class DataController {
     var selectedContact: SelectedContact?
+
+    @MainActor
+    static let previewContainer: ModelContainer = {
+        do {
+            let config = ModelConfiguration(isStoredInMemoryOnly: true)
+            let container = try ModelContainer(for: SelectedContact.self, configurations: config)
+
+            for i in 1...9 {
+                let contact = SelectedContact.sampleData
+                container.mainContext.insert(contact)
+            }
+
+            return container
+        } catch {
+            fatalError("Failed to create model container for previewing: \(error.localizedDescription)")
+        }
+    }()
 }
